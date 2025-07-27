78f249f3d107f9545dd41fa9c8f915f93dbb903f
Adhritcoolentrepenaur <adhritrana5@gmail.com>
2025-07-27 12:02:30 +0530

    Loops

---
+++ loop.py
@@ -0,0 +1,9 @@
+for i in range(1, 20):
+    print(i)
+name="Adhrit" 
+for i in name:
+    print(i)   
+l1=[1,2,3,4,5]
+for i in l1:
+    print(i)
+    
---
+++ nestedlooping.py
@@ -0,0 +1,6 @@
+l1=[["apple", "banana", "cherry"], ["date", "elderberry", "fig", "grape"]]
+l2=[["aubergine", "broccoli"], ["carrot", "daikon", "eggplant"]]
+for sublist in l1:
+    for item in sublist:
+        print(sublist, item)
+        
